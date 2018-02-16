84be80e3e9a4e7e872d948b63a2477c4dffe93ce
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-16 03:08:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-return array.join if array.length == 1
- return array.length <= 1
- return array[0..-2].join(", ") + " and " + array[-1]
+ array.join if array.length == 1
+  array.length <= 1
+  array.join(", ") + " and " + array[-1]
 end
 
 
